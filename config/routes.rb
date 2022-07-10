4a050edc92c822aa8080b1feb2fe1664e845272d
kukla1989 <kukla.1989@ukr.net>
2022-07-10 12:05:29 +0300

    add integration test, polished up routes, add conroller user

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
+  get '/signup', to: 'users#new'
   root 'static_page#home'
-  get 'static_page/help'
-  get 'static_page/about'
-
+  get '/help', to: 'static_page#help'
+  get '/about', to: 'static_page#about'
+  get '/contact', to: 'static_page#contact'
 end
--- test/controllers/static_page_controller_test.rb
+++ test/controllers/static_page_controller_test.rb
@@ -1,31 +1,27 @@
-require "test_helper"
-
-class StaticPageControllerTest < ActionDispatch::IntegrationTest
-  
-  def setup
-    @base_title = "| Ruby on Rails Tutorial Sample App"
-  end
-  
-  test "should get home from root" do
-    get root_url
+require 'test_helper'
+class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+  test "should get home" do
+    get root_path
     assert_response :success
     assert_select "title", "Ruby on Rails Tutorial Sample App"
   end
-  
-  
 
   test "should get help" do
-    get static_page_help_url
+    get help_path
     assert_response :success
-    assert_select "title", "Help #{@base_title}"
+    assert_select "title", "Help | Ruby on Rails Tutorial Sample App"
   end
 
   test "should get about" do
-    get static_page_about_url
+    get about_path
     assert_response :success
-    assert_select "title", "About #{@base_title}"
+    assert_select "title", "About | Ruby on Rails Tutorial Sample App"
   end
-  
 
-  
+  test "should get contact" do
+    get contact_path
+    assert_response :success
+    assert_select "title", "Contact | Ruby on Rails Tutorial Sample App"
+  end
+
 end
