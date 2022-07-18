6805ef293d852d7d7de3bb13fb2d90b528ecb36c
kukla1989 <kukla.1989@ukr.net>
2022-07-18 14:08:49 +0300

    add flash and integration test for valid sign up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,8 +6,9 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
-    if @user.save
-      redirect_to(user_path @user)
+    if  @user.save
+      flash[:success] = "Welcome to the sample app #{@user.name}"
+      redirect_to user_path(@user) #or just redirect_to @user
     else
       render 'new'
     end
--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -17,4 +17,18 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     assert_select "div.alert"
   end
 
+  test "valid sign up information" do
+    get signup_path
+    assert_difference 'User.count', 1 do
+      post users_path, params: {user: {name:   'roman',
+                                       email: 'roma@gj.com',
+                                       password:              'asddfdasfdaf',
+                                       password_confirmation: 'asddfdasfdaf'} }
+    end
+    follow_redirect!
+    assert_template 'users/show'
+    assert_not flash.empty?
+    assert_select "div.alert-success"
+  end
+
 end
