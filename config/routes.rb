483a952dbc1f97a09e6251f9fa4725b84a8ccfbf
kukla1989 <kukla.1989@ukr.net>
2022-07-22 10:54:25 +0300

    add new controller and routes sessions

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
+  root 'static_page#home'
   resources :users
   get '/signup', to: 'users#new'
-  root 'static_page#home'
   get '/help', to: 'static_page#help'
   get '/about', to: 'static_page#about'
   get '/contact', to: 'static_page#contact'
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
 end
