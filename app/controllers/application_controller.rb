bc8c158c99f0305556987965c8b6d117978e1d98
kukla1989 <kukla.1989@ukr.net>
2022-07-23 13:48:23 +0300

    add user_id to session

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,3 @@
 class ApplicationController < ActionController::Base
-  def hello
-    render html: "hello path to enlightenment :)"
-  end
+  include SessionsHelper
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,9 +5,11 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
-
+      log_in user
+      redirect_to user
     else
-
+      flash.now[:danger] = "invalid email or password!"
+      render 'new'
     end
   end
 
