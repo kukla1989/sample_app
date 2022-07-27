27771fccbfd42635104e23e03da3bf86f6e57c48
kukla1989 <kukla.1989@ukr.net>
2022-07-27 07:34:31 +0300

    log in auto after sign up and some tests for login and improve sign up test

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,12 +4,13 @@ class SessionsController < ApplicationController
 
   def create
     user = User.find_by(email: params[:session][:email].downcase)
-    if user && user.authenticate(params[:session][:password])
+    if user&.authenticate(params[:session][:password])
+      reset_session
       log_in user
       redirect_to user
     else
       flash.now[:danger] = "invalid email or password!"
-      render 'new'
+      render 'new', status: :unprocessable_entity
     end
   end
 
--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -1,13 +1,43 @@
 require "test_helper"
 
 class UsersLoginTest < ActionDispatch::IntegrationTest
-  test "flash after wrong login not appear on next page" do
+  def setup
+    @user = users(:roma)
+  end
+
+
+  test "login with valid email/invalid password" do
     get login_path
     assert_template 'sessions/new'
-    post login_path, params: { session: {email: 'sfda@sf', password: 'sadfasdfsadf'} }
+    post login_path, params: { session: { email:    "roma@gmail.com",
+                                          password: "invalid" } }
+    assert_response :unprocessable_entity
     assert_template 'sessions/new'
     assert_not flash.empty?
     get root_path
     assert flash.empty?
+  end
+
+
+  test "flash after wrong login not appear on next page" do
+    get login_path
+    assert_template 'sessions/new'
+    post login_path, params: { session: {email: 'sfda@sf',
+                                         password: 'sadfasdfsadf'} }
+    assert_template 'sessions/new'
+    assert_not flash.empty?
+    get root_path
+    assert flash.empty?
+  end
+
+  test "login with valid information" do
+    post login_path, params: {session: {email: @user.email,
+                                        password: 'password'}}
+    assert_redirected_to @user
+    follow_redirect!
+    assert_template 'users/show'
+    #assert_select "a[href=?]", login_path, count: 0
+    assert_select "a[href=?]", logout_path
+    assert_select "a[href=?]", user_path(@user)
   end
 end
