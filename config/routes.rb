517d59b10b7e9a6331330c17436049dd51ad3ac4
kukla1989 <kukla.1989@ukr.net>
2022-08-05 11:41:38 +0300

    add activation_account controller, add columns to users and fill users.activation_digest automatically when create user

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root 'static_page#home'
   resources :users
+  resources :account_activations, only: [:edit]
   get '/signup', to: 'users#new'
   get '/help', to: 'static_page#help'
   get '/about', to: 'static_page#about'
--- db/seeds.rb
+++ db/seeds.rb
@@ -3,14 +3,10 @@ User.create!(name: "roma",
              email: "roma@gmail.com",
              password: "kukla123",
              password_confirmation: "kukla123",
-             admin: true)
+             admin: true,
+             activated: true,
+             activated_at: Time.zone.now)
 
-User.create!(name:  "Example User",
-             email: "example@railstutorial.org",
-             password:              "foobar",
-             password_confirmation: "foobar")
-
-# Generate a bunch of additional users.
 99.times do |n|
   name  = Faker::Name.name
   email = "example-#{n+1}@gmail.com"
@@ -18,5 +14,7 @@ User.create!(name:  "Example User",
   User.create!(name:  name,
                email: email,
                password:              password,
-               password_confirmation: password)
+               password_confirmation: password,
+               activated: true,
+               activated_at: Time.zone.now)
 end
