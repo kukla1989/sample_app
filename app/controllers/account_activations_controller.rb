547a12b70e7c7f7d61b0bebfe94fc8cfa0ed555b
kukla1989 <kukla.1989@ukr.net>
2022-08-08 18:25:11 +0300

    create activation method - edit from activation_controller, change authenticated? method so now it can authenticate remember and activation

--- app/controllers/account_activations_controller.rb
+++ app/controllers/account_activations_controller.rb
@@ -1,3 +1,16 @@
 class AccountActivationsController < ApplicationController
 
+  def edit
+    user = User.find_by(email: params[:email])
+    if user && !user.activated? && user.authenticated?(:activation, params[:id])
+      user.update_attribute(:activated, true)
+      user.update_attribute(:activated_at, Time.zone.now)
+      log_in user
+      flash[:success] = "Congratulation! account activated."
+      redirect_to user
+    else
+      flash[:danger] = "Activation link is invalid, try sign up again"
+      redirect_to signup_path
+    end
+  end
 end
