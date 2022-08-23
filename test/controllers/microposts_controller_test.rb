38bc282f45ff1b49bd9a42ec4ca2217f27323265
kukla1989 <kukla.1989@ukr.net>
2022-08-23 18:05:12 +0300

    add create micropst

--- test/controllers/microposts_controller_test.rb
+++ test/controllers/microposts_controller_test.rb
@@ -1,7 +1,22 @@
 require "test_helper"
 
 class MicropostsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @micropost = microposts(:user_second)
+  end
+
+  test "should redirect to log in when try to create but not logged in" do
+    assert_no_difference 'Micropost.count' do
+      post microposts_path, params: {micropost: {content: "some content"}}
+    end
+    assert_redirected_to login_path
+  end
+
+  test "should redirect destroy when not logged in" do
+    assert_no_difference 'Micropost.count' do
+      delete micropost_path(@micropost)
+    end
+    assert_response :see_other
+    assert_redirected_to login_path
+  end
 end
