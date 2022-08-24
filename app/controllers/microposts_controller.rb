1f82a2c9eec9fe0a1d41834cbcb3c3499fca1964
kukla1989 <kukla.1989@ukr.net>
2022-08-24 12:02:48 +0300

    add delete functionality for microposts

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -1,5 +1,6 @@
 class MicropostsController < ApplicationController
   before_action :logged_in_user, only: [:create, :destroy]
+  before_action :correct_user, only: :destroy
 
   def create
     @micropost = current_user.microposts.build(micropost_params)
@@ -13,9 +14,21 @@ class MicropostsController < ApplicationController
   end
 
   def destroy
+    @micropost.destroy
+    flash[:success] = "micropost was successfully destroyed"
+    if request.referrer
+      redirect_to request.referrer, status: :see_other
+    else
+      redirect_to root_url, status: :see_other
+    end
   end
 
   private
+
+  def correct_user
+    @micropost = current_user.microposts.find_by(id: params[:id])
+    redirect_to root_url, status: :see_other unless @micropost
+  end
 
   def micropost_params
     params.require(:micropost).permit(:content)
